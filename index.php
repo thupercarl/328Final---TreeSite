a4ee935d614c0eb33d239e9cbc1028410687416b
Aubrey Davies <adavies6@mail.greenriver.edu>
2021-04-30 13:23:26 -0400

    created the basic setup files

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+// This is my controller for the Final project
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require autoload file
+require_once ('vendor/autoload.php');
+
+// Instantiate Fat-Fre
+$f3 = Base::instance();
+
+// Define default route
+$f3->route('GET /', function(){
+    // Display the home page
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+// Run Fat-Free
+$f3->run();
