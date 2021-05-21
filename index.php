990c0a3a30141690a56ff335a19c22dae5d6de35
Jake Donaldson <Jdonaldson8@mail.greenriver.edu>
2021-05-21 13:43:53 -0400

    working on signup page

--- index.php
+++ index.php
@@ -19,18 +19,35 @@ $f3->route('GET /', function(){
     echo $view->render('views/home.html');
 });
 
+// Define results route
 $f3->route('GET /results', function(){
     //Display the results page
     $view = new Template();
     echo $view->render('views/results.html');
 });
 
+// Define signup route
+$f3->route('GET /signup', function(){
+    //Display the signup page
+    $view = new Template();
+    echo $view->render('views/signup.html');
+});
+
+// Define summary route
+$f3->route('GET /summary', function(){
+    //Display the summary page
+    $view = new Template();
+    echo $view->render('views/summary.html');
+});
+
+// Define login route
 $f3->route('GET /login', function(){
     //Display the login page
     $view = new Template();
     echo $view->render('views/login.html');
 });
 
+// Define user page route
 $f3->route('GET /user', function(){
     //Display the user page
     $view = new Template();
