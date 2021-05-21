0938f6a86a924861f5ecfe13d40d9f7984101a41
Aubrey Davies <adavies6@mail.greenriver.edu>
2021-05-21 14:12:19 -0400

    did data validation in signup route

--- index.php
+++ index.php
@@ -6,8 +6,12 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+//Start a session
+session_start();
+
 // Require autoload file
 require_once ('vendor/autoload.php');
+require_once ('model/validation.php');
 
 // Instantiate Fat-Free
 $f3 = Base::instance();
@@ -27,7 +31,79 @@ $f3->route('GET /results', function(){
 });
 
 // Define signup route
-$f3->route('GET /signup', function(){
+$f3->route('GET|POST /signup', function($f3){
+
+    //Reinitialize session array
+    $_SESSION = array();
+
+    //Initialize variables for user input
+    $fname = "";
+    $lname = "";
+    $username = "";
+    $password = "";
+    $repassword = "";
+
+    //If the form has been submitted, validate the data
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //var_dump($_POST);
+
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $repassword = $_POST['repassword'];
+
+        //If fname is valid, store data
+        if(validName($_POST['fname'])) {
+            $_SESSION['fname'] = $fname;
+        }
+        //Otherwise, set an error variable in the hive
+        else {
+            $f3->set('errors["fname"]', 'Please enter a first name');
+        }
+
+        //If lname is valid, store data
+        if(validName($_POST['lname'])) {
+            $_SESSION['lname'] = $lname;
+        }
+        //Otherwise, set an error variable in the hive
+        else {
+            $f3->set('errors["lname"]', 'Please enter a last name');
+        }
+
+        //If username is valid, store data
+        if(validUsername($_POST['username'])) {
+            $_SESSION['username'] = $username;
+        }
+        //Otherwise, set an error variable in the hive
+        else {
+            $f3->set('errors["username"]', 'Please enter a username');
+        }
+
+        //If password is valid, store data
+        if(validPassword($_POST['password'])) {
+            $_SESSION['password'] = $password;
+        }
+        //Otherwise, set an error variable in the hive
+        else {
+            $f3->set('errors["password"]', 'Please enter a password');
+        }
+
+        //If password is valid, store data
+        if(validPassword($_POST['repassword'])) {
+            $_SESSION['repassword'] = $repassword;
+        }
+        //Otherwise, set an error variable in the hive
+        else {
+            $f3->set('errors["repassword"]', 'Please enter the same password');
+        }
+
+        //If the error array is empty, redirect to summary page
+        if (empty($f3->get('errors'))) {
+            header('location: summary');
+        }
+    }
+
     //Display the signup page
     $view = new Template();
     echo $view->render('views/signup.html');
--- model/validation.php
+++ model/validation.php
@@ -1,7 +1,12 @@
 <?php
 
-function validName($name) {
+function validName($name)
+{
     return strLen(trim($name)) >= 2;
+}
+
+function validUsername($username) {
+    return strLen(trim($username)) >= 2;
 }
 
 function validPassword($password) {
